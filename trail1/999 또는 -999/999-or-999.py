1712059593474f43f62677525bdf89838693f2d3
wndms1359-lgtm <wndms1359@gmail.com>
2026-09-10 09:32:12 +0900

    [Passed] 999 또는 -999 (49ms, 16MB)

--- trail1/999 또는 -999/999-or-999.py
+++ trail1/999 또는 -999/999-or-999.py
@@ -1,18 +1,14 @@
-N = list(map(int, input().split()))
-T = len(N)
-idx = 0
-for i in range(T):
-    if N[i] == (999 | -999):
-        idx = i 
+tc = list(map(int, input().split()))
+max_v = tc[0]
+min_v = tc[0]
 
-result = N[0:i]
-max_v = N[0]
-min_v = N[0]
 
-for x in result:
-    if x > max_v:
-        max_v = x
-    if x < min_v:
-        min_v = x
-    
-print(max_v, min_v) 
+for i in tc:
+    if i == 999 or i == -999:
+        break
+    if i > max_v:
+        max_v = i
+    if i < min_v:
+        min_v = i
+
+print(max_v, min_v)
