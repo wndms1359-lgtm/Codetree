7c83f322eb7467b3a84f4340998291c77c9a8a6d
wndms1359-lgtm <wndms1359@gmail.com>
2026-09-15 00:10:09 +0900

    [Passed] 특정 문자의 유무 (48ms, 16MB)

--- trail1/특정 문자의 유무/specific-character-presence.py
+++ trail1/특정 문자의 유무/specific-character-presence.py
@@ -1,18 +1,11 @@
 N = input()
 
-cnt1 = 0
-cnt2 = 0
-for i in range(len(N)-1):
-    if (N[i] + N[i+1] == "ee"):
-        cnt1 += 1
-    if (N[i] + N[i+1] == "ab"):
-        cnt2 += 1
-
-if cnt1 > 0 and cnt2 > 0 :
-    print("Yes Yes")
-elif cnt1 > 0 and cnt2 == 0:
-    print("Yes No")
-elif cnt1 == 0 and cnt2 > 0:
-    print("No Yes")
+if 'ee' in N:
+    print("Yes", end=" ")
 else:
-    print("No No")
+    print("No", end=" ")
+
+if 'ab' in N:
+    print("Yes", end=" ")
+else:
+    print("No", end= " ")
