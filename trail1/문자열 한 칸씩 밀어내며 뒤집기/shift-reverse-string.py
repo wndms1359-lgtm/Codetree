ba1bee5109e681632346142826469791512b8ca2
wndms1359-lgtm <wndms1359@gmail.com>
2026-09-16 11:35:25 +0900

    [Passed] 문자열 한 칸씩 밀어내며 뒤집기 (50ms, 16MB)

---
+++ trail1/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
@@ -0,0 +1,16 @@
+input_str, q = input().split()
+q = int(q)
+queries = [int(input()) for _ in range(q)]
+
+# Please write your code here.
+for i in queries:
+    if i == 1:
+        input_str = input_str[1:] + input_str[0]
+        print(input_str)
+    if i == 2:
+        input_str = input_str[-1] + input_str[0:-1]
+        print(input_str)
+    if i==3:
+        input_str = input_str[::-1]
+        print(input_str)
+
