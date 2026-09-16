e6d5a1ee30b2b7b5fe0bc3927df4f0afe727d66e
wndms1359-lgtm <wndms1359@gmail.com>
2026-09-16 14:10:18 +0900

    [Passed] Run Length 인코딩 (48ms, 16MB)

--- trail1/Run Length 인코딩/run-length-encoding.py
+++ trail1/Run Length 인코딩/run-length-encoding.py
@@ -2,30 +2,18 @@ A = input()
 count = 1
 answer = ""
 
-if len(A) == 1 :
-    answer = answer + A[0] + "1"
-else:
-    for i in range(1,len(A)-1):
-        if  A[i] == A[i-1]:
-            count += 1
-        else:
-            answer = answer + A[i-1] + str(count)
-            count = 1
-    
-    if A[-1] == A[-2]:
+for i in range(1,len(A)):
+    if  A[i] == A[i-1]:
         count += 1
-        answer = answer + A[-1] + str(count)
     else:
-        answer = answer + A[-2] + str(count)
-        answer = answer + A[-1] + "1"
+        answer = answer + A[i-1] + str(count)
+        count = 1
+    
+answer = answer + A[-1] + str(count)
 
 answer_length = 0
 for i in answer:
     answer_length += 1
-
-
-
-
 
 print(answer_length)    
 print(answer)
